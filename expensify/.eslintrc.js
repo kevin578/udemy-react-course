625da2cd59e6875972761ef9e212d86073a95e97
kevin578 <kbriggs@aapl000894.local>
2017-12-19 18:39:43 -0500

    header links up

---
+++ expensify/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": 0,
+        "react/prefer-stateless-function": 0,
+        "import/first": 0,
+        "no-undef": 0,
+        "class-methods-use-this": 0,
+        "no-console": 0,
+        "react/prop-types": 0,
+        "jsx-a11y/anchor-is-valid": 0,
+        "react/jsx-boolean-value": 0,
+    }
+
+
+
+
+};
