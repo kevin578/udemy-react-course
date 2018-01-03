0ce2adbbe4db8d869d4f50c1a9ed18d0c5780bc3
kevin578 <kbriggs@aapl000894.local>
2018-01-02 20:18:44 -0500

    setting up jest

--- expensify/src/components/ExpenseList.js
+++ expensify/src/components/ExpenseList.js
@@ -1,33 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
-import selectExpenses from './../selectors/expenses'
+import selectExpenses from '../selectors/expenses';
 
-const ExpenseList = (props) => {  
-    return (
-    <div>
-    <h1>Hello</h1>
-    {
-        props.expenses.map((item, index) => (
-         <ExpenseListItem 
-           key = {item.id}
-           id = {item.id}
-           description = {item.description} 
-           amount= {item.amount}
-           createdAt= {item.createdAt}
-           />
-        ))
-    }
-
+const ExpenseList = (props) => (
+  <div>
+    <h1>Expense List</h1>
+    {props.expenses.map((expense) => {
+      return <ExpenseListItem key={expense.id} {...expense} />;
+    })}
   </div>
-  )
-}
+);
 
-const mapStatetoProps = (state) => {
-    return {
-        expenses: selectExpenses(state.expenses, state.filters)
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    expenses: selectExpenses(state.expenses, state.filters)
+  };
+};
 
-
-export default connect(mapStatetoProps)(ExpenseList);
+export default connect(mapStateToProps)(ExpenseList);
